46ec780a9ea4fe02ffbed454716e0b00b6d2d6e0
li-yunjie <li-yunjie@mercari.com>
2020-02-27 22:17:37 +0900

    test

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  core.setOutput("assignee", github.context.payload.pull_request.assignee.login);
+  console.log(`Body is : ${github.context.payload.pull_request.body}`);
+  const assignee = JSON.stringify(github.context.payload.pull_request.assignee.login, undefined, 2)
+  console.log(`Assignee is ${assignee}`);
+  core.setOutput("assignee", assignee);
 } catch (error) {
   core.setFailed(error.message);
 }
