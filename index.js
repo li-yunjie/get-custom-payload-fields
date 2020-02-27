2074d248292a48cadf03027dc711fd77a0b44b2a
li-yunjie <li-yunjie@mercari.com>
2020-02-27 22:02:48 +0900

    use dot

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  core.setOutput("assignee", github.context.payload.pull_request[assignee][login]);
+  core.setOutput("assignee", github.context.payload.pull_request.assignee.login);
 } catch (error) {
   core.setFailed(error.message);
 }
