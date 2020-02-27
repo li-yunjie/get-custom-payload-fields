4d2086689cb73d1e365ff46b7a154bd8e38b024b
li-yunjie <li-yunjie@mercari.com>
2020-02-27 21:45:56 +0900

    test

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  core.setOutput("assignee", payload.pull_request.assignee.login);
 } catch (error) {
   core.setFailed(error.message);
 }
