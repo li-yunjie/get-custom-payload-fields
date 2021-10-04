23ad72351bff91adae1032f1a81f1dac7f4cb375
Atsushi Araki <araki_atsushi@mercari.com>
2021-10-04 18:29:05 +0900

    if PR does not have assignee, prevent crash from error of Cannot read property 'login' of null

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const assignee = JSON.stringify(github.context.payload.pull_request.assignee.login)
+  const assignee = JSON.stringify(github.context.payload.pull_request.assignee?.login)
   console.log(`The assignee is ${assignee}`);
   core.setOutput("assignee", assignee);
 } catch (error) {
