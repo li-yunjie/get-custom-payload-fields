bf353a5c7604da078132768b7a531aeee30bfec0
Atsushi Araki <araki_atsushi@mercari.com>
2021-10-04 22:46:06 +0900

    check null

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log(`======================`);
-  console.log(`assignee is ${github.context.payload.pull_request.assignee}`)
-  console.log(`======================`);
-  console.log(`login is ${github.context.payload.pull_request.assignee.login}`)
-  console.log(`======================`);
-  if (github.context.payload.pull_request.assignee === undefined) {
-    console.log(`The assignee is undefined`);
+  if (github.context.payload.pull_request.assignee === null) {
+    console.log(`The assignee is null`);
     core.setOutput("assignee", null);
   } else {
     const assignee = JSON.stringify(github.context.payload.pull_request.assignee.login)
